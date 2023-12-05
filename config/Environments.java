b8e362a34f076adb492e34c1f89889850c2f815d
grover <gcupariacarapi@gmail.com>
2023-12-05 17:00:40 -0400

    examen

---
+++ config/Environments.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Environments {
+    public static String lag = "en";
+    public static String pathFile = "/parcial2/app/data/user.txt";
+    public static String pathFileProducts = "/parcial2/app/data/product.txt";
+}
